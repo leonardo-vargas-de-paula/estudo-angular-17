b454dbd34a76b77576b8693a94d430bd50ae07b4
Leonardo Vargas <leonardovargasdpaula@gmail.com>
2025-03-28 13:55:19 -0400

    feat: adiciona card para o produto

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,10 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { ProductService } from '../../shared/services/product.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
