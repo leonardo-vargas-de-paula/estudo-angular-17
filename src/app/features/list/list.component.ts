63e6801ddef833bc56732623b562ef1b4fd5a80c
Leonardo Vargas <leonardovargasdpaula@gmail.com>
2025-03-28 13:37:14 -0400

    feat: exibe produtos utilizando service

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { ProductService } from '../../shared/services/product.service';
 
 @Component({
   selector: 'app-list',
@@ -7,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+  httpClient = inject(HttpClient);
+  products: any[] = [];
 
+  productsSevice = inject(ProductService);
+
+  ngOnInit() {
+    this.productsSevice.getAll().subscribe((products)=>{
+      this.products=products;
+    })
+  }
 }
